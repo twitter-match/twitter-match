b9b142d3b5713e0eaf147b3455bca4c05c8fe3a2
twitter-match <twittermatchalbert@gmail.com>
2019-07-07 18:33:48 +0900

    pep8

--- src/twitter.py
+++ src/twitter.py
@@ -2,6 +2,7 @@ import os
 import json
 from requests_oauthlib import OAuth1Session
 
+
 class Twitter():
 
     GET_USERS_FRIENDS_IDS_URL = 'https://api.twitter.com/1.1/friends/ids.json'
@@ -11,7 +12,7 @@ class Twitter():
     GET_USERS_FAVORITES_URL = 'https://api.twitter.com/1.1/favorites/list.json'
     GET_USERS_LISTS_URL = 'https://api.twitter.com/1.1/lists/list.json'
     GET_USERS_TWEETS_URL = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
-    
+
     def __init__(self):
 
         self.API_KEY = os.getenv('API_KEY')
@@ -23,9 +24,9 @@ class Twitter():
             self.API_SECRET_KEY,
             self.ACCESS_TOKEN,
             self.ACCESS_TOKEN_SECRET)
-        
+
     def create_user(self, user_id):
-        
+
         return User(self.session, user_id)
 
     def check_response(self, response):
@@ -63,36 +64,42 @@ class Twitter():
         return objects
 
 
-class User(Twitter): 
-    
+class User(Twitter):
+
     def __init__(self, session, user_id):
 
         self.session = session
         self.user_id = user_id
-    
+
     def get_friends_ids(self, stringify_ids=True, max_count=None, **kwargs):
 
         url = self.GET_USERS_FRIENDS_IDS_URL
-        params = {'user_id': self.user_id, 'stringify_ids': stringify_ids, **kwargs}
-        self.friends_ids = self.get_objects(url, params, key='ids', max_count=max_count)
+        params = {'user_id': self.user_id,
+                  'stringify_ids': stringify_ids, **kwargs}
+        self.friends_ids = self.get_objects(
+            url, params, key='ids', max_count=max_count)
 
     def get_followers_ids(self, stringify_ids=True, max_count=None, **kwargs):
 
         url = self.GET_USERS_FOLLOWERS_IDS_URL
-        params = {'user_id': self.user_id, 'stringify_ids': stringify_ids, **kwargs}
-        self.followers_ids = self.get_objects(url, params, key='ids', max_count=max_count)
+        params = {'user_id': self.user_id,
+                  'stringify_ids': stringify_ids, **kwargs}
+        self.followers_ids = self.get_objects(
+            url, params, key='ids', max_count=max_count)
 
     def get_friends_list(self, max_count=None, **kwargs):
 
         url = self.GET_USERS_FRIENDS_LIST_URL
         params = {'user_id': self.user_id, **kwargs}
-        self.friends_list = self.get_objects(url, params, key='users', max_count=max_count)
+        self.friends_list = self.get_objects(
+            url, params, key='users', max_count=max_count)
 
     def get_followers_list(self, max_count=None, **kwargs):
 
         url = self.GET_USERS_FOLLOWERS_LIST_URL
         params = {'user_id': self.user_id, **kwargs}
-        self.followers_list = self.get_objects(url, params, key='users', max_count=max_count)
+        self.followers_list = self.get_objects(
+            url, params, key='users', max_count=max_count)
 
     def get_favorites(self, **kwargs):
 
