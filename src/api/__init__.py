2426928eab732defed831c762b4dc92bbfc276ed
twitter-match <twittermatchalbert@gmail.com>
2019-07-11 18:18:18 +0900

    rename params.yml into consts.yml

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -4,7 +4,7 @@ import yaml
 from api import api
 
 yaml_path = os.path.join(
-    os.path.dirname(__file__), 'params.yml')
+    os.path.dirname(__file__), 'consts.yml')
 with open(yaml_path, 'r') as f:
     APIS_CONSTS = yaml.load(f, Loader=yaml.FullLoader)
 
